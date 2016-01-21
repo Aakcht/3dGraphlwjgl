260ffb188e077ed4e8fa549234b9ed671235b515
aakcht <aakcht@gmail.com>
2016-01-21 23:33:24 +0400

    Test commit

--- src/andrey/vizualization/graph/FunctionToDraw.java
+++ src/andrey/vizualization/graph/FunctionToDraw.java
@@ -7,7 +7,7 @@ public class FunctionToDraw {
     public static double valueInPoint(XYZCoord xyzCoord)
     {
         //return xyzCoord.x*xyzCoord.x+xyzCoord.y*xyzCoord.y+xyzCoord.z*xyzCoord.z;
-        //return Math.sin(10*(xyzCoord.x*xyzCoord.x+xyzCoord.y*xyzCoord.y))/10-xyzCoord.z+1 ;
+//return Math.sin(10*(xyzCoord.x*xyzCoord.x+xyzCoord.y*xyzCoord.y))/10-xyzCoord.z+1 ;
         return Math.sin(5*xyzCoord.x) * Math.cos(5*xyzCoord.y)/5-xyzCoord.z+1;
 //        return  Math.sin(xyzCoord.x)+Math.sin(xyzCoord.y)+Math.sin(xyzCoord.z)+
 //                Math.cos(xyzCoord.x)+Math.cos(xyzCoord.y)+Math.cos(xyzCoord.z)-2.2+1;
